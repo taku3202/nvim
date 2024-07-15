a968d5a7ecc716ad467ed34ddbb2bc3a45e5d4ab
Takuya Higuchi <thiguchi1114@gmail.com>
2024-07-15 10:42:29 +0900

    add save on formatter

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -142,4 +142,36 @@ return {
       require("telescope").load_extension('frecency')
     end,
   },
+
+  -- save on formatter
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.prettier,
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({
+                  async = false,
+                  filter = function(c)
+                    return c.name == "null-ls"
+                  end,
+                })
+              end,
+            })
+          end
+        end,
+      })
+    end
+  }
 }
